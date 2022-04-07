22fbbacbef98104d53e035646e4f59b97ea628b3
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2022-04-07 21:38:48 +0430

    coupons for users

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,8 +1,59 @@
-<style scoped></style>
+<style scoped>
+.coupon {
+    position: relative;
+    max-width: 290px;
+    min-height: 10rem;
+    z-index: 0;
+    background-image: url("/misc/coupon.svg");
+    background-position: center;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    filter: drop-shadow(0px 15px 15px rgba(0, 0, 0, 7%));
+}
+.coupon span {
+    letter-spacing: 2px;
+}
+.coupon > div {
+    max-width: 200px;
+}
+
+.discount_tag {
+    border-radius: 0 0 1rem 1rem;
+}
+
+.porteqal {
+    position: absolute;
+    right: -3.25rem;
+    transform: rotate(90deg);
+}
+</style>
 
 <template>
     <div class="relative flex flex-col gap-8 w-full max-w-screen-lg flex-grow">
         <ActiveRoadmap v-if="!loading" />
+
+        <section class="flex flex-col gap-2 w-full rounded-3xl" v-if="!loading && !!coupons.length" id="availabe-coupon-codes">
+            <div class="flex items-center gap-2 mb-4">
+                <img src="/icons/admin/Discount.svg" width="32" height="32" alt="Discount" />
+                <h2 class="kalameh_bold title text-2xl">کدهای تخفیف</h2>
+            </div>
+            <div v-swiper:couponsSwiper="couponsSwiperOptions" class="relative w-full select-none overflow-hidden">
+                <ul class="swiper-wrapper relative flex w-full h-full flex-grow">
+                    <li class="coupon swiper-slide flex flex-col items-center justify-center w-full" v-for="(coupon, i) in coupons" :key="i">
+                        <img class="porteqal w-28" src="/misc/half.png" alt="porteqal" />
+                        <div class="absolute top-2 left-4 flex flex-col gap-2 items-end w-full">
+                            <b class="discount_tag kalameh_black text-3xl p-2 bg-rose-600 text-amber-200">
+                                {{ coupon.amount }}
+                                <span v-if="coupon.amountType == 'percent'">%</span>
+                                <span v-else>تومان</span>
+                            </b>
+                            <span class="kalameh_black text-2xl text-white">{{ coupon.code }}</span>
+                            <small class="text-xs text-left opacity-80">مهلت استفاده تا تاریخ {{ new Date(coupon.endDate).toLocaleString("fa") }}</small>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
 
         <section class="flex flex-col gap-2 shadow-xl bg-white w-full p-4 md:p-6 rounded-3xl" v-if="!loading" id="purchased-courses">
             <div class="flex items-center gap-2 mb-4">
@@ -78,6 +129,14 @@ export default {
         return {
             loading: true,
 
+            coupons: [],
+            couponsLoading: false,
+            couponsSwiperOptions: {
+                autoplay: false,
+                slidesPerView: "auto",
+                loop: false,
+            },
+
             courses: [],
             coursesLoading: false,
         };
@@ -86,7 +145,7 @@ export default {
         let headers = {};
         if (process.server) headers = this.$nuxt.context.req.headers;
 
-        await Promise.all([this.getCourses({ headers })]);
+        await Promise.all([this.getCourses({ headers }), this.getCouponCodes({ headers })]);
     },
     mounted() {
         this.loading = false;
@@ -113,6 +172,24 @@ export default {
                 .then((results) => (this.courses = results.data.records))
                 .catch((e) => {})
                 .finally(() => (this.coursesLoading = false));
+        },
+
+        async getCouponCodes(data = {}) {
+            if (this.couponsLoading) return;
+            this.couponsLoading = true;
+
+            let url = `/api/user-profile/coupons`;
+            let headers = {};
+            if (process.server) {
+                url = `${process.env.BASE_URL}${url}`;
+                headers = data.headers ? data.headers : {};
+            }
+
+            await axios
+                .get(url, { headers })
+                .then((results) => (this.coupons = results.data.records))
+                .catch((e) => {})
+                .finally(() => (this.couponsLoading = false));
         },
     },
 };
